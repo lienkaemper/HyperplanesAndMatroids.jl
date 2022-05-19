8e2280fa85fc3607d561f9c663b6cfb04d499a3f
Documenter.jl <documenter@juliadocs.github.io>
2022-05-19 16:22:24 +0000

    build based on 954bdd3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HyperplanesAndMatroids","category":"page"},{"location":"#HyperplanesAndMatroids","page":"Home","title":"HyperplanesAndMatroids","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HyperplanesAndMatroids.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HyperplanesAndMatroids]","category":"page"}]
+}
